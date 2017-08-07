0dedd478d6c1293c21a5ce95207133b869fa0f12
Stuart Slade <stuartslade@contino.io>
2017-08-07 14:52:01 +0100

    another minor change

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'OK, looking extremely good',
+      message: 'OK, looking amazingly good',
     }),
   };
 
